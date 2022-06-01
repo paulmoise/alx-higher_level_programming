5f6ac3b84fa46689fdf1789416a5b9b9db08454d
enigmatik100 <paul.gangbadja@erugis.fr>
2022-06-02 00:32:31 +0100

    8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-s1 = f"{str[39:66]} {str[-22:-18]} {str[:7]}"
-str = s1
+str = f"{str[39:66]} {str[-22:-18]} {str[:7]}"
 print(str)
