23e85bd95d0a8c9a879072a7eb975a03da54abfc
enigmatik100 <paul.gangbadja@erugis.fr>
2022-06-03 08:35:25 +0100

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    from sys import argv
+    if len(argv) == 2:
+        print(f"{len(argv) - 1} argument:")
+    elif len(argv) == 1:
+        print(f"{len(argv) - 1} arguments.")
+    else:
+        print(f"{len(argv) - 1} arguments:")
+    for i in range(1, len(argv)):
+        print(f"{i}: {argv[i]}")
