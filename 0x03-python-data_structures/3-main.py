e3c3c4b16a2f102593961c99d76eea2438e669e9
enigmatik100 <paul.gangbadja@erugis.fr>
2022-06-04 21:11:44 +0100

    3-print_reversed_list_integer.py

--- 0x03-python-data_structures/3-main.py
+++ 0x03-python-data_structures/3-main.py
@@ -3,3 +3,5 @@ print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_
 
 my_list = [1, 2, 3, 4, 5]
 print_reversed_list_integer(my_list)
+second_list = []
+print_reversed_list_integer(second_list)
--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,6 +3,6 @@
 
 def print_reversed_list_integer(my_list=[]):
     """ Function that prints all integers of a list, in reverse"""
-
-    for idx in range(len(my_list) - 1, -1, -1):
-        print('{:d}'.format(my_list[idx]))
+    my_list.reverse()
+    for element in my_list:
+        print('{:d}'.format(element))
