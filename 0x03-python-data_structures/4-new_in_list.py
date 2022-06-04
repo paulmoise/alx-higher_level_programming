32174244cac8bf7391fa3aca69d6174ca9eee053
enigmatik100 <paul.gangbadja@erugis.fr>
2022-06-04 21:25:45 +0100

    4-new_in_list.py

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,24 @@
+#!/usr/python3
+
+
+def new_in_list(my_list, idx, element):
+    """
+    function that replaces an element in a list at specific position without
+    modifying the original list
+    ...
+    Parameters
+    ----------
+    my_list : list
+        The list of elements
+    idx : integer
+        The given position
+    element : the new element
+    Return:
+        The original list if idx is negative or
+        updated list
+    """
+    if isinstance(my_list, list):
+        new_list = my_list.copy()
+        if 0 <= idx < len(my_list):
+            new_list[idx] = element
+        return new_list
