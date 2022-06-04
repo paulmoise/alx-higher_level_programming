e033cd4367b3b093ed55c727a30dc94d475295e2
enigmatik100 <paul.gangbadja@erugis.fr>
2022-06-04 23:31:00 +0100

    6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,5 +6,7 @@ def print_matrix_integer(matrix=[[]]):
 
     for row in range(len(matrix)):
         for col in range(len(matrix[0])):
-            print('{:d}'.format(matrix[row][col]), end=" ")
+            print("{:d}".format(matrix[row][col]), end="")
+            if col != len(matrix[0]) - 1:
+                print(" ", end="")
         print()
