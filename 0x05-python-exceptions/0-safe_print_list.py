bf49b70d690bfdeb21d9675f7ef11505a897ac02
enigmatik100 <paul.gangbadja@erugis.fr>
2022-06-21 00:16:46 +0100

    Python - Exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-
 def safe_print_list(my_list=[], x=0):
     """
     Function that prints x elements of a list using try/ catch
@@ -8,8 +7,9 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     try:
         for i in range(x):
-            print(my_list[i])
+            print(my_list[i], end="")
             count += 1
+        print()
         return count
     except IndexError:
         return count
