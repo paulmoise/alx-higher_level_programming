d02928bbd64be599bb9f9c7f0eaf69461189f0fd
enigmatik100 <paul.gangbadja@erugis.fr>
2022-06-21 22:01:50 +0100

    2-safe_print_list_integers

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def safe_print_list_integers(my_list=[], x=0):
+    """
+    Function that prints the first x elements of a list and only integers.
+    """
+    count = 0
+    try:
+        for i in range(x):
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        print()
+        return count
+    except (IndexError, ValueError):
+        print()
+        return count
