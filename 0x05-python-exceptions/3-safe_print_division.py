6210a7003b7ef5ea1a86c3e15bc26d84d524b66f
enigmatik100 <paul.gangbadja@erugis.fr>
2022-06-21 22:59:27 +0100

    3-safe_print_division

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def safe_print_division(a, b):
+    """
+    Function that divides 2 integers and prints the result
+    """
+    res = None
+    try:
+        res = a/b
+    except ZeroDivisionError:
+        pass
+    finally:
+        print("Inside result: {}".format(res))
