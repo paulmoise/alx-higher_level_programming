b7bf3ae28dfc36044df2b2b317165583d31bec25
enigmatik100 <paul.gangbadja@erugis.fr>
2022-06-22 01:24:41 +0100

    100-safe_print_integer_err

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import sys
 
 
 def safe_print_integer_err(value):
@@ -8,6 +9,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as e:
-        print("Exception:{}".format(e))
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr)
         return False
