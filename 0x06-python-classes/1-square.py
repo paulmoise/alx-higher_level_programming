44ee0c46b7c184b187d65cbbe82f40ce37fc5a4b
enigmatik100 <paul.gangbadja@erugis.fr>
2022-06-25 14:04:57 +0100

    0-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Square with one attribute"""
+
+
+class Square:
+    """ Square class with size attribute"""
+
+    def __init__(self, size):
+        """
+            function to initialize square with it's size
+        """
+        self.__size = size
+        
