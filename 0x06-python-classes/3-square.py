e2a6c37ebc64f8b2be721477f40954e5c9aefaa3
enigmatik100 <paul.gangbadja@erugis.fr>
2022-06-25 14:28:23 +0100

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Square with one attribute"""
+
+
+class Square:
+    """ Square class with size attribute"""
+
+    def __init__(self, size=0):
+        """
+            function to initialize square with it's size
+        """
+        if type(size) == int:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """ Function that returns the current square area """
+        return (self.__size ** 2)
