ca0567eb7231d5f8da1594d3ecb15b269e5fc3af
enigmatik100 <paul.gangbadja@erugis.fr>
2022-06-26 15:04:04 +0100

    4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+""" Square with one attribute"""
+
+
+class Square:
+    """ Square class with size attribute"""
+
+    def __init__(self, size=0):
+        """
+            Function to initialize square with it's size
+        """
+        if type(size) == int:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """ Function that returns the current square area
+
+            Args:
+                self: the active instance of the class
+
+            Returns:
+                 int: return the area of the square
+        """
+        return (self.__size ** 2)
+
+    @property
+    def size(self):
+        """ size getter
+
+            Args:
+                 self: the active instance of the class
+
+            Returns:
+                    int: size of the area
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ Setter of size attribute
+
+            Args:
+                self: the active instance of the class
+                value: the value to be set
+
+            Returns:
+                    nothing
+        """
+
+        if type(value) == int:
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
+        else:
+            raise TypeError("size must be an integer")
