85decef0ed83c73c711add4cc341b5413c9eb519
enigmatik100 <m.p.gangbadja@gmail.com>
2022-11-20 13:37:46 +0100

    peak find

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,12 +1,31 @@
 #!/usr/bin/python3
 """Defines a peak-finding algorithm."""
 
+
+def search(nums, left, right):
+    if left == right:
+        return nums[left]
+    mid = int((left + right) / 2)
+    if nums[mid] >= nums[mid + 1]:
+        return search(nums, right, mid)
+    return search(nums, mid + 1, right)
+
+
 def find_peak(list_of_integers):
-	
-	peak = None
-	
-	for i in range(len(list_of_integers)):
-		if list_of_integers[i] >= list_of_integers[i + 1]:
-			return list_of_integers[i]
-	return list_of_integers[-1]
-	
+    """
+        - Brute force algorith will lead to O(nlog(n)),
+        we will just sort the array and find the maximum
+
+        - Better approach is to use binary search.
+
+        if the active value is greater than the next,
+        then the peak is in the left part of the list
+
+        if not, the peak is in the second part
+        we stop when the left and right index are the same
+
+        complexity: O(log(n))
+    """
+    if not list_of_integers:
+        return None
+    return search(list_of_integers, 0, len(list_of_integers) - 1)
