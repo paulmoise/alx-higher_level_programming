d4749e87b613d30641d6959a34dde957f8f46321
enigmatik100 <m.p.gangbadja@gmail.com>
2022-11-20 09:10:33 +0100

    error fixed in first task

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,19 +1,32 @@
 #!/usr/bin/python3
+""" This module lists all the states from the
+    database hbtn_0e_0_usa.
 """
-Script that lists all states from the database hbtn_0e_0_usa.
-"""
+
 import MySQLdb
 import sys
 
+
+def main():
+    """
+        Function containing code to select all the states
+        from the database.
+    """
+
+    # Create a database connection
+    conn = MySQLdb.connect(
+                host="localhost", port=3306, user=sys.argv[1],
+                passwd=sys.argv[2], db=sys.argv[3], charset="utf8mb4"
+            )
+    cur = conn.cursor()
+    # Select states
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
+
+
 if __name__ == "__main__":
-    db = MySQLdb.connnect(host="localhost", user=sys.argv[1], port=3306,
-                          password=sys.argv[2], db=sys.argv[3])
-    cursor = db.cursor()
-    sql = "SELECT id, name FROM states ORDER BY id ASC"
-    cursor.execute(sql)
-    states = cursor.fetchall()
-    if states is not None:
-        for row in states:
-            print("({}, '{}')".format(row[0], row[1]))
-        cursor.close()
-        db.close()
+    main()
