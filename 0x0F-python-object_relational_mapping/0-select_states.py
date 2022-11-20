0ed08d82306d34b06f7ca21d73ef7ec9f5ea8c73
enigmatik100 <m.p.gangbadja@gmail.com>
2022-11-20 09:04:24 +0100

    error fixed in first task

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-"""lists all states from the database hbtn_0e_0_usa
- Usage: ./0-select_states.py <mysql username>
-                            <mysql password>
-                            <database name >
 """
-
-from sys import argv
+Script that lists all states from the database hbtn_0e_0_usa.
+"""
 import MySQLdb
+import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=argv[1], password=argv[2], db=argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states` ORDER BY `id` ASC")
-    [print(state) for state in c.fetchall()]
-    c.close()
-    db.close()
+    db = MySQLdb.connnect(host="localhost", user=sys.argv[1], port=3306,
+                          password=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    sql = "SELECT id, name FROM states ORDER BY id ASC"
+    cursor.execute(sql)
+    states = cursor.fetchall()
+    if states is not None:
+        for row in states:
+            print("({}, '{}')".format(row[0], row[1]))
+        cursor.close()
+        db.close()
