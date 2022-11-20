f6a561b83b7859a4180a253b4560f596277fa44d
enigmatik100 <m.p.gangbadja@gmail.com>
2022-11-20 08:36:12 +0100

    object mapping project

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """lists all states from the database hbtn_0e_0_usa
+use mysql driver first
  Usage: ./0-select_states.py <mysql username>
                             <mysql password>
-                            <database name >
+                            <database name>
 """
 
 import sys
